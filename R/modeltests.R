7ce500a4af321d66f9c2d778ed3fa9015e3736ac
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-03-01 22:06:03 +0100

    update

--- R/modeltests.R
+++ R/modeltests.R
@@ -47,7 +47,11 @@ modeltests<-function(dir=".", model=NULL){
   source("start.R")
 
   runcode<- paste0("-AMT-.*.202[1-9]-[0-1][0-9]-",format(Sys.Date(),"%d"))
-  while(!any(grepl(runcode,system("squeue -u giannou -h -o '%i %q %T %C %M %j %V %L %e %Z'",intern=TRUE) ))) print("bla")
-  mifs <- system("find /p/projects/remind/modeltests/ -name REMIND*mif",intern=TRUE)
-  a <- quitte::read.quitte()
+  repeat {
+    print("in")
+    if(!any(grepl(runcode,system("squeue -u giannou -h -o '%i %q %T %C %M %j %V %L %e %Z'",intern=TRUE) ))) break
+  }
+print("out")
+  #  mifs <- system("find /p/projects/remind/modeltests/ -name REMIND*mif",intern=TRUE)
+#  a <- quitte::read.quitte()
 }
