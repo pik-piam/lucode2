adb1c90a4d727b931dc2a9b518620078285624d2
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-03-02 20:12:28 +0100

    remove direct function calls, add link to git testing_suite in the email for easier report access

--- R/modeltests.R
+++ R/modeltests.R
@@ -39,7 +39,7 @@ modeltests<-function(dir=".",gitdir=NULL, model=NULL,user=NULL){
   # Did all the runs start?
   amtcsv<-read.csv2(paste0(dir,"/config/scenario_config_AMT.csv"))
   start<-NULL
-  amtcsv<-dplyr::filter(amtcsv,start==1)
+  amtcsv <- filter(amtcsv,start==1)
   if (length(paths) < length(amtcsv[,"start"]) ) out["Runs"]<-"Some runs did not start"
 
   # Which runs finished properly?
@@ -48,13 +48,14 @@ modeltests<-function(dir=".",gitdir=NULL, model=NULL,user=NULL){
   mifs <- grep("_withoutPlus",mifs,invert = TRUE, value = TRUE)
   if (length(mifs) < length(amtcsv[,"start"]) ) out["mifs"]<-"Some runs did not write a reporting file"
 
-  a<-quitte::read.quitte(mifs)
-  out[["iamCheck"]]<-iamc::iamCheck(a,model)
+  a <- read.quitte(mifs)
+  out[["iamCheck"]] <- iamCheck(a,model)
 
-
-#  write.csv(c(date(),out[["Runs"]]),file = "README.md")
+  setwd(gitdir)
+  linkgit <- system("git config --get remote.origin.url",intern = TRUE)
+  setwd(dir)
   rdm<-readLines(paste0(gitdir,"/README_temp.md"))
-  rdm <- sub("Date: .*",paste0("Date: ",date()),rdm)
+  rdm <- sub("Date: .*",paste0("Date: ",date(),". Direct link to this report: ",linkgit),rdm)
   rdm <- sub("Tested Commit: .*",paste0("Tested commit: ",system("git log -1",intern=TRUE)[[1]]),rdm)
   if (modelinerror) {
    rdm <- sub(paste0(model," is in .*"),paste0(model, " is in ERROR"),rdm)
