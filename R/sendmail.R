03812115e94e312180f0397ec71fa4d912faec48
giannou <11047746+giannou@users.noreply.github.com>
2021-03-05 14:58:48 +0100

    add a commit message argument to sendmail

--- R/sendmail.R
+++ R/sendmail.R
@@ -5,10 +5,12 @@
 #' 
 #' @param org path to the clone of the gitlab repo
 #' @param file absolute path to the file to be commited and pushed
+#' @param commitmessage the commit message
+#' (appears in the subject line of the email that will be sent)
 #' @author Anastasis Giannousakis
 #' @export 
 
-sendmail <- function(org,file) {
+sendmail <- function(org,file,commitmessage) {
 
   if (!grepl("^/",file)) stop("'file' has to be an absolute path!")
   goback<-getwd()
@@ -16,8 +18,8 @@ sendmail <- function(org,file) {
   system("git reset --hard origin/master &&
           git pull")
   file.copy(file,"README.md",overwrite=T)
-  system("git commit -m 'mycommit' README.md &&
-          git push")
+  system(paste0("git commit -m '",commitmessage,"' README.md &&
+          git push"))
   setwd(goback)
 
 }
