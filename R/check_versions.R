b6f91ecb0971ca80f2b409b55ca78a9a94ac47b5
giannou <11047746+giannou@users.noreply.github.com>
2021-03-08 13:36:34 +0100

    add a function to check if CRAN packages exist with same name and newer version than our RSE packages

---
+++ R/check_versions.R
@@ -0,0 +1,31 @@
+#' Library cleanup tool
+#' 
+#' Checks if there are CRAN-packages with the same name as those in our PIK-CRAN whose version is newer
+#' 
+#' @param mail whether an email notification is sent to RSE
+#' @author Anastasis Giannousakis
+#' @export
+#' @importFrom utils available.packages old.packages
+#' @examples
+#' 
+#' \dontrun{check_versions()}
+#' 
+check_versions <- function(mail=TRUE) {
+  
+  a<-rownames(available.packages("https://rse.pik-potsdam.de/r/packages/src/contrib"))
+  
+  cran<-rownames(old.packages(repos=getOption("repos")["CRAN"]))
+  
+  newer <- intersect(a,cran)
+  file <- paste0(tempdir(),"/README.md")
+
+  if (length(newer)!=0 ) {
+    cat("WARNING\n")
+    writeLines(paste0("WARNING, following packages have a newer version number on CRAN: ",newer),file)
+    if (mail) sendmail("/p/projects/rd3mod/R/libraries/compCRANtoRSE/",file,commitmessage("WARNING"))
+  } else {
+    cat("No CRAN packages with newer versions than our RSE packages found\n")
+  }
+
+}
+
