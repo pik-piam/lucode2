ae782d98da3fa403f06fd5aad252519d686c8247
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-05-14 12:29:28 +0200

    remove .lintr config stuff

--- R/lint.R
+++ R/lint.R
@@ -12,34 +12,9 @@
 #' @examples
 #' lucode2::lint()
 #' @export
-lint <- function(filesToLint = getFilesToLint(), useLintrConfigFile = FALSE) {
-  if (useLintrConfigFile) {
-    # Using a config file instead of explicitly setting linter config here (in code) has the advantage of also being
-    # respected when linting this project in another way, for example lintr::lint(path) or devtools::lint().
-    # Also, a project could customize its .lintr to better fit the project's needs. However, this could lead to
-    # different quality standards in different repos, which contradicts the purpose of lucode2::buildLibrary, which
-    # tries to achieve a similar software quality in all repos.
-    # TODO if we want to go this route, add documentation about the .lintr config file
-    gitRoot <- system("git rev-parse --show-toplevel", intern = TRUE)
-    linterConfigPath <- paste0(gitRoot, "/.lintr")
-    if (!file.exists(linterConfigPath)) {
-      cat(paste("linters: with_defaults(",
-                "  line_length_linter = line_length_linter(120),",
-                '  object_name_linter = object_name_linter(styles = "camelCase"),',
-                "  cyclocomp_linter = NULL",
-                "  )\n", sep = "\n"), file = linterConfigPath)
-
-      rbuildignorePath <- paste0(gitRoot, "/.Rbuildignore")
-      cat("^\\.lintr$\n", file = rbuildignorePath, append = TRUE)
-
-      system(paste("git add", linterConfigPath, rbuildignorePath))
-      cat("Created linter config file '.lintr' and added it to '.Rbuildignore'. Please commit these files.\n")
-    }
-    return(sapply(filesToLint, lintr::lint))
-  } else {
-    linters <- lintr::with_defaults(line_length_linter = lintr::line_length_linter(120),
-                                    object_name_linter = lintr::object_name_linter(styles = "camelCase"),
-                                    cyclocomp_linter = NULL)
-    return(sapply(filesToLint, lintr::lint, linters = linters))
-  }
+lint <- function(filesToLint = getFilesToLint()) {
+  linters <- lintr::with_defaults(line_length_linter = lintr::line_length_linter(120),
+                                  object_name_linter = lintr::object_name_linter(styles = "camelCase"),
+                                  cyclocomp_linter = NULL)
+  return(sapply(filesToLint, lintr::lint, linters = linters))
 }
