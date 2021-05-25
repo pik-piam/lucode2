c5d3b98b86e9c24ec14b17ea5af4e89823e343b3
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-05-25 12:05:01 +0200

    remove autoFormat question from buildLibrary

--- R/autoFormat.R
+++ R/autoFormat.R
@@ -2,7 +2,9 @@
 #'
 #' Apply auto-formatting using styler::style_file to the given files.
 #'
-#' @param filesToAutoFormat A character vector of paths to files that should be auto-formatted.
+#' @param files A character vector of paths to files that should be auto-formatted.
+#' @param ignoreLintFreeFiles If set to TRUE only files with linter warnings are auto-formatted.
+#' @param lintAfterwards If set to TRUE return linter results of the auto-formatted files.
 #'
 #' @author Pascal Führlich
 #' @seealso \code{\link{getFilesToLint}}
@@ -12,6 +14,12 @@
 #' lucode2::autoFormat()
 #' }
 #' @export
-autoFormat <- function(filesToAutoFormat = getFilesToLint()) {
-  style_file(filesToAutoFormat)
+autoFormat <- function(files = getFilesToLint(), ignoreLintFreeFiles = TRUE, lintAfterwards = TRUE) {
+  if (ignoreLintFreeFiles) {
+    files <- files[sapply(files, function(aFile) length(lint(aFile)) > 0)]
+  }
+  style_file(files)
+  if (lintAfterwards) {
+    return(lint(files))
+  }
 }
