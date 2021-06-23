cfa9e10726604bb439af79ea2ac0741aeac7149c
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-06-23 16:29:35 +0200

    remove path linter rule, split up buildLibrary

--- R/addGitHubActions.R
+++ R/addGitHubActions.R
@@ -9,6 +9,7 @@
 #' is only created, if missing and can be edited manually.
 #'
 #' @param lib Path to the package
+#' @param removeTravis If TRUE try to remove existing travis configuration and files.
 #'
 #' @author Jan Philipp Dietrich
 #' @seealso \code{\link{buildLibrary}}
@@ -18,22 +19,50 @@
 #' addGitHubActions()
 #' }
 #' @export
-addGitHubActions <- function(lib = ".") {
-  standardactionfile <- paste0(lib, "/.github/workflows/test-buildlibrary.yaml")
+addGitHubActions <- function(lib = ".", removeTravis = TRUE) {
+  standardactionfile <- file.path(lib, ".github", "workflows", "test-buildlibrary.yaml")
   if (file.exists(standardactionfile)) {
     file.remove(standardactionfile)
   }
-  usethis::use_github_action(url = system.file("extdata/test-buildlibrary.yaml", package = "lucode2"),
-                             save_as = "test-buildlibrary.yaml")
-  if (!file.exists(paste0(lib, "/codecov.yml"))) {
+  usethis::use_github_action(
+    url = system.file(file.path("extdata", "test-buildlibrary.yaml"), package = "lucode2"),
+    save_as = "test-buildlibrary.yaml"
+  )
+  if (!file.exists(file.path(lib, "codecov.yml"))) {
     usethis::use_coverage("codecov")
-    file.copy(system.file("extdata/codecov.yml", package = "lucode2"), paste0(lib, "/codecov.yml"), overwrite = TRUE)
+    file.copy(system.file(file.path("extdata", "codecov.yml"), package = "lucode2"),
+              file.path(lib, "codecov.yml"),
+              overwrite = TRUE)
   }
-  testfolder <- paste0(lib, "/tests/testthat")
+  testfolder <- file.path(lib, "tests", "testthat")
   if (!file.exists(testfolder)) {
     dir.create(testfolder, recursive = TRUE)
   }
   if (length(dir(testfolder)) == 0) {
-    writeLines('skip("dummy test")', paste0(testfolder, "/test-dummy.R"))
+    writeLines('skip("dummy test")', file.path(testfolder, "test-dummy.R"))
+  }
+
+  if (removeTravis) {
+    # remove travis related parts
+    travisfile <- file.path(lib, ".travis.yml")
+    if (file.exists(travisfile)) {
+      file.remove(travisfile)
+      rbuildignore <- file.path(lib, ".Rbuildignore")
+      if (file.exists(rbuildignore)) {
+        rbuildignoreContent <- readLines(rbuildignore)
+        writeLines(grep("travis", rbuildignoreContent, value = TRUE, invert = TRUE), rbuildignore)
+      }
+      testfolder <- file.path(lib, "tests", "testthat")
+      if (!file.exists(testfolder)) {
+        dir.create(testfolder, recursive = TRUE)
+      }
+      travistest <- file.path(lib, "tests", "testthat", "test-travisCI.R")
+      if (file.exists(travistest)) {
+        file.remove(travistest)
+      }
+      if (length(dir(testfolder) == 0)) {
+        writeLines('skip("dummy test")', file.path(testfolder, "test-dummy.R"))
+      }
+    }
   }
 }
