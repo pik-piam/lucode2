301c4913bdba3131d3ff90c285ae39b946466a58
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-10-18 16:15:53 +0200

    fix usethis 2.1 incompatibility

--- R/addGitHubActions.R
+++ R/addGitHubActions.R
@@ -1,9 +1,9 @@
 #' addGitHubActions
 #'
-#' adds GitHubActions with a standard workflow including codecov to the package. It will add a standard
-#' Github action "test-buildlibrary.yml" to the project which performs standard tests as well as
-#' creates a coverage report. This file is overwritten automatically each time this function is run and
-#' should not be edited by hand. Additional Github actions can be added as separate files.
+#' This function adds a standard Github action workflow called "lucode2-check.yaml" to the project which runs
+#' lucode2::check(), checks the validation key, and creates a coverage report using codecov. This file is overwritten
+#' automatically each time this function is run and should not be edited by hand. Additional Github actions can be
+#' added as separate files.
 #'
 #' In addition, this function adds a codecov.yml to the repository, if not already existing. This file
 #' is only created if missing and can be edited manually.
@@ -12,16 +12,17 @@
 #'
 #' @author Jan Philipp Dietrich
 #' @seealso \code{\link{buildLibrary}}
-#' @importFrom usethis use_github_action use_coverage
+#' @importFrom usethis use_github_file use_coverage
 #' @examples
 #' \dontrun{
 #' addGitHubActions()
 #' }
 #' @export
 addGitHubActions <- function(lib = ".") {
-  unlink(file.path(lib, ".github", "workflows", "test-buildlibrary.yaml"))
+  unlink(file.path(lib, ".github", "workflows", "test-buildlibrary.yaml")) # the old workflow, remove this at some point
   unlink(file.path(lib, ".github", "workflows", "lucode2-check.yaml"))
-  use_github_action("lucode2-check.yaml", system.file(file.path("extdata", "lucode2-check.yaml"), package = "lucode2"))
+  use_github_file("https://github.com/pik-piam/lucode2/blob/master/.github/workflows/lucode2-check.yaml",
+                  save_as = file.path(".github", "workflows", "lucode2-check.yaml"))
 
   if (!file.exists(file.path(lib, "codecov.yml"))) {
     use_coverage("codecov")
