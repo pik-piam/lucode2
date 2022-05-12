8b2243a8c850eaf23681f0721e1b7fc1e8d9b58c
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-05-12 12:01:09 +0200

    add find dependencies tool

---
+++ R/findDeps.R
@@ -0,0 +1,25 @@
+#' findDeps
+#'
+#' Find all dependencies of an R project.
+#'
+#' This is a wrapper around `renv::dependencies()` that does not report
+#' dependencies on core R packages, because these are always available.
+#'
+#' @param devDeps Whether development dependencies should also be checked.
+#' @return A dataframe documenting which dependency is needed where.
+#' @author Pascal Führlich
+#' @seealso \code{\link[renv]{dependencies}}
+#' @importFrom utils installed.packages
+#' @examples
+#' findDeps()
+#' @export
+findDeps <- function(devDeps = TRUE) {
+  checkRequiredPackages("renv")
+  # let renv auto-detect dependencies, including dev dependencies
+  deps <- renv::dependencies(dev = devDeps)
+  # remove packages that are part of core R
+  corePackages <- installed.packages(priority = "base")[, "Package"]
+  deps <- deps[!deps$Package %in% corePackages, ]
+  cat(paste0('c("', paste0(sort(unique(deps$Package)), '"', collapse = ',\n  "'), ")\n"))
+  return(deps)
+}
