081083a78c060610e23ed47f7ae5a6bdb27a911b
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-06-13 15:43:38 +0200

    check adds package documentation

--- R/check.R
+++ R/check.R
@@ -26,6 +26,13 @@
 #' @export
 check <- function(lib = ".", cran = TRUE, config = loadBuildLibraryConfig(lib), runLinter = TRUE) {
   lib <- normalizePath(lib, winslash = "/")
+
+  packageName <- desc(file.path(lib, "DESCRIPTION"))$get("Package")
+  packageDocumentation <- file.path(lib, "R", paste0(packageName, "-package.R"))
+  if (!file.exists(packageDocumentation)) {
+    writeLines(c("#' @docType package", '"_PACKAGE"'), packageDocumentation)
+  }
+
   document(pkg = lib, roclets = c("rd", "collate", "namespace", "vignette"))
 
   ########### Run tests ###########
