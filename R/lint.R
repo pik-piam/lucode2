773b97855aff9243ec1525b495d87de35eab8960
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-06-22 15:51:00 +0200

    rework lucode2::lint

--- R/lint.R
+++ R/lint.R
@@ -2,8 +2,9 @@
 #'
 #' Check the given files for linter warnings using lintr::lint.
 #'
-#' For files in the vignettes and tests folder less strict rules are applied, e.g. using ::: usually leads to a linter
-#' warning, but not in vignettes/tests.
+#' For files in the vignettes and tests folder less strict rules are applied, e.g. using ::: usually
+#' leads to a linter warning, but not in vignettes/tests. Which linter rules are used depends on
+#' ".lintr" config files. \code{\link{buildLibrary}} creates lintr config files that use \code{\link{lintrRules}}.
 #'
 #' @param files A character vector of paths to files that should be checked by the linter. If set to "."
 #' the whole package is linted.
@@ -11,7 +12,7 @@
 #' linter warnings.
 
 #' @author Pascal Führlich
-#' @seealso \code{\link{getFilesToLint}}, \code{\link{autoFormat}}, \code{\link[lintr]{lint}}
+#' @seealso \code{\link{lintrRules}}, \code{\link{getFilesToLint}}, \code{\link{autoFormat}}, \code{\link[lintr]{lint}}
 #' @importFrom lintr lint_package with_defaults absolute_path_linter line_length_linter object_name_linter
 #' todo_comment_linter undesirable_function_linter cyclocomp_linter default_undesirable_functions
 #' undesirable_operator_linter default_undesirable_operators T_and_F_symbol_linter
@@ -19,45 +20,15 @@
 #' lucode2::lint()
 #' @export
 lint <- function(files = getFilesToLint()) {
-  # names = deprecated functions, values = replacement hint
-  deprecatedFunctions <- list(
-    `on.exit` = "use withr::defer",
-    fulldim = "use magclass::getItems()",
-    getRegionList = "use magclass::getItems()",
-    getRegions = "use magclass::getItems()",
-    `getRegions<-` = "use magclass::getItems()"
-  )
-
-  # in vignettes amd tests undesirable functions (except deprecated ones) and undesirable operators are ok
-  linterArgsForVignettesAndTests <- list(
-    absolute_path_linter = absolute_path_linter(),
-    line_length_linter = line_length_linter(120),
-    object_name_linter = object_name_linter(styles = "camelCase"),
-    todo_comment_linter = todo_comment_linter(),
-    undesirable_function_linter = undesirable_function_linter(deprecatedFunctions),
-    cyclocomp_linter = cyclocomp_linter(25),
-    T_and_F_symbol_linter = T_and_F_symbol_linter
-  )
-  lintersForVignettesAndTests <- do.call(with_defaults, linterArgsForVignettesAndTests)
-
-  linterArgs <- linterArgsForVignettesAndTests
-  linterArgs$undesirable_function_linter <- # nolint
-    undesirable_function_linter(c(default_undesirable_functions, deprecatedFunctions))
-  linterArgs$undesirable_operator_linter <- # nolint
-    undesirable_operator_linter(c(default_undesirable_operators, list(`->` = NA)))
-  linters <- do.call(with_defaults, linterArgs)
-
   if (identical(files, ".")) {
-    files <- list.files(path = c("tests", "R", "inst"), pattern = "\\.R(md|nw)?$", recursive = TRUE, full.names = TRUE)
+    return(lint_package())
   }
 
   files <- normalizePath(files)
-  # use lintersForVignettesAndTests instead of linters if file is in vignetteAndTestFiles
-  vignetteAndTestFiles <- normalizePath(list.files(path = c("tests", "vignettes"), recursive = TRUE, full.names = TRUE))
 
   linterWarnings <- lapply(files, function(aFile) {
-    message('Running lucode2::lint("', aFile, '")')
-    return(lintr::lint(aFile, linters = if (aFile %in% vignetteAndTestFiles) lintersForVignettesAndTests else linters))
+    message('Running lintr::lint("', aFile, '")')
+    return(lintr::lint(aFile))
   })
 
   # combine the results of multiple calls to lintr::lint, taken from lintr:::flatten_lints
@@ -75,6 +46,7 @@ lint <- function(files = getFilesToLint()) {
     assignItem(x)
     return(structure(res, class = "lints"))
   }
+
   linterWarnings <- flattenLints(linterWarnings)
   class(linterWarnings) <- "lints"
   return(linterWarnings)
--- R/lintrRules.R
+++ R/lintrRules.R
@@ -3,24 +3,29 @@
 #' undesirable_operator_linter default_undesirable_operators T_and_F_symbol_linter
 #' @export
 lintrRules <- function(allowUndesirable = FALSE) {
+  # names = deprecated functions, values = replacement hint
+  deprecatedFunctions <- list(`on.exit` = "use withr::defer",
+                              # by default on.exit replaces all code registered to be run on exit,
+                              # making it very dangerous to use on.exit together with withr functions
+                              fulldim = "use magclass::getItems()",
+                              getRegionList = "use magclass::getItems()",
+                              getRegions = "use magclass::getItems()",
+                              `getRegions<-` = "use magclass::getItems()")
+
   linters <- linters_with_defaults(absolute_path_linter(),
                                    line_length_linter(120),
                                    object_name_linter(styles = "camelCase"),
                                    todo_comment_linter(),
                                    cyclocomp_linter(25),
-                                   T_and_F_symbol_linter)
+                                   T_and_F_symbol_linter())
 
-  deprecatedFunctions <- list(`on.exit` = "use withr::defer",
-                               fulldim = "use magclass::getItems()",
-                               getRegionList = "use magclass::getItems()",
-                               getRegions = "use magclass::getItems()",
-                               `getRegions<-` = "use magclass::getItems()")
   if (allowUndesirable) {
-    linters <- c(linters, undesirable_function_linter(deprecatedFunctions))
+    linters$undesirable_function_linter <- undesirable_function_linter(deprecatedFunctions)
   } else {
-    linters <- c(linters,
-                 undesirable_operator_linter(c(default_undesirable_operators, list(`->` = NA))),
-                 undesirable_function_linter(c(default_undesirable_functions, deprecatedFunctions)))
+    linters$undesirable_operator_linter <- undesirable_operator_linter(c(default_undesirable_operators,
+                                                                         list(`->` = NA)))
+    linters$undesirable_function_linter <- undesirable_function_linter(c(default_undesirable_functions,
+                                                                         deprecatedFunctions))
   }
   return(linters)
 }
