72160f603d17bd570f133790685be115ccb318d6
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-09-21 14:56:08 +0200

    bugfix in manipulateConfig - variables which had another variable name as part of their name were incorrectly replaced

--- R/manipulateConfig.R
+++ R/manipulateConfig.R
@@ -30,7 +30,7 @@ manipulateConfig <- function(configFile, ...) {
   }
   m <- list()
   type <- substring(configFile, nchar(configFile) - 2)
-  if (type == "gms" | type == "inc") {
+  if (type %in% c("gms", "inc")) {
     for (i in names(tmp)) {
       if (length(tmp[[i]]) > 1) {
         rpl <- capture.output(cat(paste0(tmp[[i]], ", "), fill = maxchar))
@@ -45,7 +45,7 @@ manipulateConfig <- function(configFile, ...) {
       m[[paste(i, "_pattern1", sep = "")]][2] <- paste(
         "\\1 ", rpl, "\\2", sep = "")
       m[[paste(i, "_pattern2", sep = "")]][1] <- paste(
-        "((\\n|^)[\\t ]*(scalar|parameter|set|)s?[\\t ]*", i, "(|\\([^\\)]*\\))[\\t ]*(\"[^\"]*\"|)[^\"/;]*/)[^/]*",
+        "((\\n|^)[\\t ]*(scalar|parameter|set|)s?[\\t ]*", i, "(|\\([^\\)]*\\))[\\t ]+(\"[^\"]*\"|)[^\"/;]*/)[^/]*",
         sep = "")
       m[[paste(i, "_pattern2", sep = "")]][2] <- paste(
         "\\1 ", rpl, " ", sep = "")
@@ -54,13 +54,13 @@ manipulateConfig <- function(configFile, ...) {
       m[[paste(i, "_pattern3", sep = "")]][2] <- paste(
         "\\1", rpl, sep = "")
     }
-  } else if (type == "cfg" | type == "R" | type == "r") {
+  } else if (type %in% c("cfg", "R", "r")) {
     for (i in names(tmp)) {
       if (is.character(tmp[[i]])) tmp[[i]] <- paste("\"", tmp[[i]], "\"", sep = "")
       m[[i]][1] <- paste("((^|[\\t \\$])", i, "[ \\t]*<-[ \\t]*)[\"\']?[^\"\']*[\"\']?", sep = "")
       m[[i]][2] <- paste("\\1", tmp[[i]], sep = "")
     }
-  } else if (type == "cmd" | type == ".sh") {
+  } else if (type %in% c("cmd", ".sh")) {
     for (i in names(tmp)) {
       m[[i]][1] <- paste("((^|[\\t ])", i, "[ \\t]*=[ \\t]*[\"\']?)[^\"\'\\n]*", sep = "")
       m[[i]][2] <- paste("\\1", tmp[[i]], sep = "")
--- tests/testthat/test-manipulateConfig.R
+++ tests/testthat/test-manipulateConfig.R
@@ -3,10 +3,12 @@ test_that("manipulateConfig works", {
   withr::with_tempdir({
     configFile <- "test.gms"
 
-    cfg <- list(foo  = 0,
-                bar  = 0,
-                buzz = 0,
-                bazz = 0)
+    cfg <- list(foo      = 0,
+                bar      = 0,
+                buzz     = 0,
+                bazz     = 0,
+                bla_blub = 0,
+                bla      = 0)
 
     code <- function(cfg) {
       x <- c("scalars",
@@ -14,10 +16,13 @@ test_that("manipulateConfig works", {
              '  bar  "either x/y or X/Y"',
              '  bazz "just x/y"',
              paste0('  buzz "not x/y" / ', cfg$buzz, " /"),
+             paste0("bla_blub example text (1) / ", cfg$bla_blub,  " /"),
              ";",
              paste0("bar  = ", cfg$bar, ";"),
              paste0("bazz = ", cfg$bazz, ";"),
-             paste0("buzz = ", cfg$buzz, ";"))
+             paste0("buzz = ", cfg$buzz, ";"),
+             paste0("bla_blub = ", cfg$bla_blub, ";"),
+             paste0("bla = ", cfg$bla, ";"))
       return(x)
     }
 
