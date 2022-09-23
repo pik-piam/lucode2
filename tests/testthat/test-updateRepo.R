b8451ec4803ccbfff3713486cbaf0d41a554cec9
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-09-23 09:46:05 +0100

    fix test

--- tests/testthat/test-updateRepo.R
+++ tests/testthat/test-updateRepo.R
@@ -12,15 +12,22 @@ test_that("updateRepo works", {
     }, "invalid validation key")
 
     writeLines("ValidationKey: '173196000'", file.path(reposFolder, "miniPackage", ".buildlibrary"))
-    updateRepo(reposFolder)
+    updateRepo(reposFolder, clean = FALSE)
     expect_true(file.exists(file.path(reposFolder, "miniPackage_0.0.0.9000.tar.gz")))
     expect_true(file.exists(file.path(reposFolder, "PACKAGES")))
 
-    expect_message(updateRepo(reposFolder), "miniPackage 0.0.0.9000 ok", fixed = TRUE)
+    expect_message(updateRepo(reposFolder, clean = FALSE), "miniPackage 0.0.0.9000 ok", fixed = TRUE)
     usethis::with_project(file.path(reposFolder, "miniPackage"), {
       usethis::use_version("major")
     })
     writeLines("ValidationKey: '1924400'", file.path(reposFolder, "miniPackage", ".buildlibrary"))
-    expect_message(updateRepo(reposFolder), "miniPackage 0.0.0.9000 -> 1.0.0 build success", fixed = TRUE)
+    expect_message(updateRepo(reposFolder, clean = FALSE),
+                   "miniPackage 0.0.0.9000 -> 1.0.0 build success", fixed = TRUE)
+    expect_message({
+      # clean = TRUE will delete untracked .buildlibrary file with validationkey
+      expect_error(updateRepo(reposFolder, clean = TRUE),
+                   "There were errors, see log (on RSE server with `journalctl -r -u update-repo.service`)",
+                   fixed = TRUE)
+    }, "invalid validation key")
   })
 })
