788d9e81f46f81fcf0f603ebe8bac5e4a7225fee
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-09-28 13:07:01 +0200

    Add working test for getScenNames

---
+++ tests/testthat/test-getScenNames.R
@@ -0,0 +1,27 @@
+
+test_that("getScenNames works", {
+  withr::with_tempdir({
+    # set up testing environment
+    dir.create("output1")
+    dir.create("output2")
+    dir.create("output3")
+    cfg <- list("Name 1", "value")
+    names(cfg) <- c("title", "otherKey")
+    save(cfg, file = "output1/config.Rdata")
+    cfg <- list("value")
+    names(cfg) <- c("title")
+    save(cfg, file = "output2/config.Rdata")
+    cfg <- list("Name 3")
+    names(cfg) <- c("title")
+    save(cfg, file = "output3/config.Rdata")
+
+    expected <- c("Name 1", "Name 2", "Name 3")
+    names(expected) <- c("output1/config.Rdata",
+                         "output2/config.Rdata",
+                         "output3/config.Rdata")
+    expect_identical(
+      expected,
+      getScenNames(c("output1", "output2", "output3"))
+    )
+  })
+})
