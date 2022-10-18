9740f9d36dcdb729fa6413cc5a1ac2c6cb0d86fe
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-10-18 16:41:52 +0200

    checkDeps improvements

--- R/checkDeps.R
+++ R/checkDeps.R
@@ -9,10 +9,10 @@
 #' @param descriptionFile Path to a DESCRIPTION or a path that belongs to a source package project.
 #' @param dependencyTypes The types of depencies to check. Must be a
 #' subset of c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances").
-#' @return Invisibly, a named logical vector indicating whether a package requirement is met.
+#' @return Invisibly, a named logical vector indicating whether each package requirement is met.
 #' @author Pascal Führlich
 #' @examples
-#' checkDeps()
+#' checkDeps(system.file("DESCRIPTION", package = "lucode2"))
 #' @export
 checkDeps <- function(descriptionFile = ".", dependencyTypes = c("Depends", "Imports", "LinkingTo")) {
   stopifnot(all(dependencyTypes %in% c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances")))
@@ -23,19 +23,22 @@ checkDeps <- function(descriptionFile = ".", dependencyTypes = c("Depends", "Imp
 }
 
 checkRequirement <- function(package, version) {
-  requiredVersion <- as.package_version(if (version == "*") "0.0" else gsub("[^0-9.]", "", version))
-  packageVersion <- if (package == "R") {
-    as.package_version(paste(R.version[c("major", "minor")], collapse = "."))
-  } else {
-    tryCatch({
-      utils::packageVersion(package)
-    }, error = function(error) NA)
+  requiredVersion <- package_version(if (version == "*") "0.0" else gsub("[^0-9.]", "", version))
+
+  packageVersion <- tryCatch({
+    utils::packageVersion(package)
+  }, error = function(error) NA)
+
+  if (package == "R") {
+    packageVersion <- getRversion()
   }
+
   if (is.na(packageVersion)) {
     warning(package, " is required, but not installed - please install it.")
   } else if (packageVersion < requiredVersion) {
     warning(package, " >= ", requiredVersion, " is required, but ", packageVersion,
             " is installed - please update.")
   }
+
   return(!is.na(packageVersion) && packageVersion >= requiredVersion)
 }
