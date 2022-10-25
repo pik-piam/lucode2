a9f9d8bf806dbd8fc058ad1225a98be750fc97fd
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2022-10-25 16:06:38 +0200

    add checkDeps ask

--- R/checkDeps.R
+++ R/checkDeps.R
@@ -11,10 +11,11 @@
 #'   - `"stop"`:  Issue an error with the unmet dependencies.  (Default.)
 #'   - `"warn"`:  Issue a warning with the unmet dependencies.
 #'   - `"pass"`:  Do nothing, just return invisibly.
+#'   - `"ask"`: Ask the user whether to auto-fix missing dependencies. Requires an active renv.
 #' @return Invisibly, a named list of strings indicating whether each package
 #'   requirement is met (`"TRUE"`) or not, in which case the reason is stated.
 #'
-#' @author Pascal Führlich
+#' @author Pascal Führlich, Michaja Pehl
 #'
 #' @examples
 #' checkDeps(system.file("DESCRIPTION", package = "lucode2"))
@@ -26,18 +27,31 @@ checkDeps <- function(descriptionFile = ".",
                       dependencyTypes = c("Depends", "Imports", "LinkingTo"),
                       action = "stop") {
   stopifnot(all(dependencyTypes %in% c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances")))
-  stopifnot(action %in% c("stop", "warn", "pass"))
+  stopifnot(action %in% c("stop", "warn", "pass", "ask"))
   allDeps <- desc::desc_get_deps(descriptionFile)
   deps <- allDeps[allDeps$type %in% dependencyTypes, ]
   requirementMet <- Map(checkRequirement, package = deps$package, version = deps$version)
 
-  if (!all("TRUE" == requirementMet)) {
-    message <- paste(requirementMet[which("TRUE" != requirementMet)],
-                     collapse = "\n  ")
-    if ("stop" == action) {
-      stop(message)
-    } else if ("warn" == action) {
-      warning(message)
+  if (!all(requirementMet == "TRUE")) {
+    missing <- requirementMet[requirementMet != "TRUE"]
+    msg <- paste(missing, collapse = "\n  ")
+    if (action == "ask" && any(grepl(": ", missing))) {
+      if (!requireNamespace("renv", quietly = TRUE) || is.null(renv::project())) {
+        stop("'action = ask' is only supported in an activated renv.")
+      }
+      message("missing requirements:\n  ", msg, "\n",
+              "Try to fix automatically? (y/n) ", appendLF = FALSE)
+      if (tolower(getLine()) %in% c("y", "yes")) {
+        renvInstall <- sub("^.+: ([^:]+)$", "\\1", grep(": ", missing, value = TRUE))
+        renv::install(renvInstall, prompt = FALSE)
+        # after installing, check again in new R session to avoid using already loaded packages
+        return(callr::r(function(...) lucode2::checkDeps(...),
+                        list(descriptionFile, dependencyTypes, action = "stop")))
+      }
+    } else if (action %in% c("stop", "ask")) {
+      stop(msg)
+    } else if (action == "warn") {
+      warning(msg)
     }
   }
 
@@ -56,11 +70,10 @@ checkRequirement <- function(package, version) {
 
   # if the package is not installed
   if (is.na(packageVersion)) {
-    result <- paste(package,
-                    "is required, but not installed - please install it.")
+    result <- paste0(package, " is required, but not installed - please install: ", package)
   # if we don't care about the version
-  } else if ("*" == version) {
-    result <- TRUE
+  } else if (version == "*") {
+    result <- "TRUE"
   # compare version requirement
   } else {
     validops <- c("<", "<=", ">", ">=", "==", "!=")
@@ -74,22 +87,41 @@ checkRequirement <- function(package, version) {
                                                ")"),
                               text = version))
     )
+    operator <- opANDv[[1]]
+    requiredVersion <- package_version(opANDv[[2]])
 
     # compare to installed version
-    if (opANDv[1] %in% validops) {
-      if (getFunction(name = opANDv[1])(packageVersion,
-                                            package_version(opANDv[2]))) {
-        result <- TRUE
+    if (operator %in% validops) {
+      if (getFunction(operator)(packageVersion, requiredVersion)) {
+        result <- "TRUE"
         # warn on mismatch
       } else {
-        result <- paste(package, opANDv[1], package_version(opANDv[2]),
-                      "is required, but", packageVersion,
-                      "is installed - install a compatible version.")
+        result <- paste(package, operator, requiredVersion,
+                        "is required, but", packageVersion,
+                        "is installed - please ")
+        if (operator %in% c(">", ">=", "!=")) {
+          result <- paste0(result, "update: ", package)
+          # renv::install(package)
+        } else if (operator %in% c("<=", "==")) {
+          result <- paste0(result, "install: ", package, "@", requiredVersion)
+          # renv::install(paste0(package, "@", requiredVersion))
+        } else { # operator == "<"
+          result <- paste0(result, "install a compatible version of ", package)
+        }
       }
     # catch faulty declarations
     } else {
-      result <- paste("invalid dependency declaration:", package, version)
+      result <- paste("invalid dependency declaration", package, version)
     }
   }
-  return(as.character(result))
+  return(result)
+}
+
+# copied from gms::getLine to prevent dependency on gms
+getLine <- function() {
+  if (interactive()) {
+    # needed for e.g. RStudio and R in jupyter
+    return(readline())
+  }
+  return(readLines(withr::local_connection(file("stdin")), n = 1))
 }
