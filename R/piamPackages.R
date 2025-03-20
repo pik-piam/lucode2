e4328d8fa03e4e724104480a227dd36a15e1df10
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2025-03-20 14:41:37 +0100

    piamPackages and build

--- R/piamPackages.R
+++ R/piamPackages.R
@@ -7,16 +7,20 @@
 #' @export
 piamPackages <- function() {
   packagesUrl <- "https://pik-piam.r-universe.dev/src/contrib/PACKAGES"
-  fallback <- c("blackmagicc", "citation", "demystas", "edgeTransport", "edgeTrpLib",
-                "GDPuc", "gdx", "gdxrrw", "gms", "goxygen", "HARr", "iamc", "limes",
-                "lpjclass", "lpjmlkit", "lucode2", "luplot", "luscale", "lusweave",
-                "m4fsdp", "madrat", "magclass", "magpie4", "MagpieNCGains", "magpiesets",
-                "mip", "modelstats", "mrcommons", "mrdieter", "mrdrivers", "mredgebuildings",
-                "mrfable", "mrfeed", "mrfish", "mrland", "mrmagpie", "mrremind",
-                "mrsoil", "mrtutorial", "mrvalidation", "mrvalidnitrogen", "mrwaste",
-                "mrwater", "mrwaterplots", "mstools", "piamenv", "piamInterfaces",
-                "piamModelTests", "piktests", "quitte", "r2databus", "regressionworlddata",
-                "remind2", "remulator", "rmndt", "rOpenscmRunner", "shinyresults",
+  fallback <- c("biospheremetrics", "blackmagicc", "boundaries", "brick", "citation",
+                "demystas", "edgeTransport", "edgeTrpLib", "GDPuc", "gdx", "gdx2",
+                "gdxrrw", "ggtrace", "gms", "goxygen", "HARr", "iamc", "limes",
+                "lpjclass", "lpjmlkit", "lpjmlstats", "lucode2", "luplot", "luscale",
+                "lusweave", "m4fsdp", "madrat", "magclass", "magpie4", "MagpieNCGains",
+                "magpiesets", "mip", "modelstats", "mrcommons", "mrdieter", "mrdrivers",
+                "mredgebuildings", "mrfable", "mrfactors", "mrfaocore", "mrfeed",
+                "mrfish", "mrindustry", "mrland", "mrlandcore", "mrmagpie", "mrorganic",
+                "mrremind", "mrsoil", "mrtransport", "mrtutorial", "mrvalidation",
+                "mrvalidnitrogen", "mrwaste", "mrwater", "mrwaterplots", "mstools",
+                "piamenv", "piamInterfaces", "piamModelTests", "piamPlotComparison",
+                "piamutils", "piamValidation", "piktests", "quitte", "r2databus",
+                "regressionworlddata", "remind2", "remulator", "reportbrick",
+                "reporttransport", "rmndt", "rOpenscmRunner", "shinyresults",
                 "SPARQL", "trafficlight")
   return(tryCatch({
     sort(sub("^Package: ", "", grep("^Package: ", readLines(packagesUrl), value = TRUE)))
