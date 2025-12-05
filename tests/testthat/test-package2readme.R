c6fb309555f0c603383e4f817ec7075d9c58f3c2
Patrick R <hi@patrickrein.de>
2025-12-05 17:16:29 +0100

    In readme generation remove newlines from project titles

--- tests/testthat/test-package2readme.R
+++ tests/testthat/test-package2readme.R
@@ -1,12 +1,26 @@
 test_that("readme can be created from package", {
   expect_invisible(r <- package2readme("lucode2"))
-  expect_gt(length(grep("lucode2", r)), 0)
-  expect_gt(length(grep("Dietrich", r)), 0)
-  expect_gt(length(grep("Installation", r)), 0)
-  expect_gt(length(grep("version", r)), 0)
-  expect_gt(length(grep("CRAN status", r)), 0)
-  expect_gt(length(grep("BibTeX", r)), 0)
-  expect_gt(length(grep("install", r)), 0)
   expect_gt(length(r), 30)
   expect_gt(sum(nchar(r)), 1500)
+
+  r <- paste0(r, collapse = "\n")
+  expect_match(r, "lucode2")
+  expect_match(r, "Dietrich")
+  expect_match(r, "Installation")
+  expect_match(r, "version")
+  expect_match(r, "CRAN status")
+  expect_match(r, "BibTeX")
+  expect_match(r, "install")
+})
+
+test_that("readme title does not contain newlines", {
+  local_mocked_bindings(
+    desc = function(...) {
+      originalDesc <- desc::desc(...)
+      originalDesc$set("Title", "a\nzZzZz \nc")
+      return(originalDesc)
+    }
+  )
+  expect_invisible(r <- package2readme("lucode2"))
+  expect_match(paste0(r, collapse = "\n"), "a zZzZz c")
 })
